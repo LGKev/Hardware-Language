263da0b1a01dc922e183a4fd0d1f41f1d4a4d1d0
kevin kuwata-think <keivnkuwata@icloud.com>
2017-10-19 11:38:22 -0600

    test

---
+++ Top_Module_project1.v
@@ -0,0 +1,15 @@
+module Top_Module_project1(SW, HEX);
+	// GLOBALS
+	//reg mode = 0; //maybe this is how we would select modes
+	
+	// INPUT
+
+	// OUTPUT
+	
+	
+	
+	
+	
+	seven_segment SevenSeg0(.sw(SW), .HEX0(HEX0));
+	
+endmodule
